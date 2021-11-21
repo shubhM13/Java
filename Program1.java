5540cdbbd78227dc41aebb521d51b1785e75350d
smishra.shubhammishra <shubham.mishra@nestle.com>
2021-11-21 17:52:19 +0530

    code push

---
+++ Program1.java
@@ -0,0 +1,27 @@
+//--- JAVA DEMO ----
+// SINGLE LINE COMMENT IN JAVA
+
+/*
+THIS IS
+A MULTILINE
+COMMENT 
+IN JAVA
+*/
+
+// A JAVA PROGRAM ALWAYS CONTAINS ATLEAST ONE CLASS => JAVA IS A PURELY OBJECT-ORIENTED LANGUAGE
+//                                                                      ------------------------
+// java utility library: Provides functionality to read inputs and write outputs
+//import java.util.*;
+
+// access specifiers : Public, Private & Protected
+public class Program1 {
+    // 1) attributes
+    // no attribute for this basic class
+
+    // 2) functions
+    // Main Function : psvm
+    public static void main(String args[]){
+      System.out.println("Hello World!");
+    }
+}
+
---
+++ Program2.java
@@ -0,0 +1,69 @@
+/*
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+
+* * * * *
+* * * *
+* * * 
+* *
+*
+
+        *
+      * * *
+    * * * * *
+  * * * * * * *
+* * * * * * * * *
+
+*/
+import java.util.*;
+
+public class Program2 {
+    //attributes
+
+    //methods
+
+    // Main Method
+    public static void main(String args[]){
+
+        //intantiation of the classs
+        Program2 object = new Program2();
+        object.firstPattern();
+        System.out.println();
+        object.secondPatten();
+        System.out.println();
+        object.thirdPattern();
+    }
+
+    //1) firstPattern()
+    public void firstPattern(){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+    }
+
+    //2) secondPatten()
+    public void secondPatten(){
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+
+    //3) thirdPattern()
+    public void thirdPattern(){
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  ***** ");
+        System.out.println(" *******");
+        System.out.println("*********");
+    }
+
+}
